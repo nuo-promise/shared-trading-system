4acd04ee9bb9b160cd92894b02708803457bc4e9
nuo-promise <nuoyan@apache.org>
2022-04-20 10:51:48 +0800

    #11 Nacos discover and config complete

--- shared-trad-user-serv/shared-trad-user-impl/src/main/java/cn/suparking/user/Application.java
+++ shared-trad-user-serv/shared-trad-user-impl/src/main/java/cn/suparking/user/Application.java
@@ -5,14 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackages = {"cn.suparking"})
 @ComponentScan(basePackages = {"cn.suparking"})
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"cn.suparking"})
