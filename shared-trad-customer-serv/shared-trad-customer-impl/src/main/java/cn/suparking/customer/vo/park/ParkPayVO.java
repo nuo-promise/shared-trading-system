9b27dcc31dc0e483d4738d92cc05c1b80662ab4b
nuo-promise <nuoyan@apache.org>
2022-08-22 12:28:19 +0800

    修改项目信息中计费简称

--- shared-trad-customer-serv/shared-trad-customer-impl/src/main/java/cn/suparking/customer/vo/park/ParkPayVO.java
+++ shared-trad-customer-serv/shared-trad-customer-impl/src/main/java/cn/suparking/customer/vo/park/ParkPayVO.java
@@ -22,8 +22,11 @@ public class ParkPayVO {
     // 免费分钟数.
     private Integer freeMinutes;
 
-    // 一天封顶金额
-    private Integer perCharge;
+    // 单位
+    private String ruleType;
+
+    // 封顶金额
+    private String perCharge;
 
     // 计费规则大概描述
     private String chargeContent;
